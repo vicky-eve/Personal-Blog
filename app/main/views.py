800cebe80d098cafb1d71a17a4d2b855f02826bf
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 20:54:17 +0300

    routemethod for email subscription added

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
 from flask_login import login_required,current_user
 from models import User, Blog, Quote,Subscribe
 from .forms import UpdateProfile,BlogForm,CommentForm,UpdateBlog,SubscribeForm
 from ..request import get_quotes
+
 
 @main.route('/user/<uname>')
 @login_required
@@ -25,8 +26,7 @@ def index():
 
         new_subscriber=Subscribe(email=email)
         new_subscriber.save_subscriber()
-        mail_message("You are now subscribed to BlogAway","email/subscribe",new_subscriber.email,new_subscriber=new_subscriber)
-        flash('Sucessfully subscribed')
+        flash('Successfull subscription!')
         return redirect(url_for('main.index'))
     
     return render_template('index.html', blogs = blogs,quotes =quotes,user=current_user, form = form)
