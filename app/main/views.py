a3689582431291bea7a4b86597f6b93a5839c661
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 21:09:22 +0300

    created route method for creating a new blog

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
 from flask_login import login_required,current_user
-from models import User, Blog, Quote,Subscribe
+from models import User, Blog, Quote,Subscribe, Comment
 from .forms import UpdateProfile,BlogForm,CommentForm,UpdateBlog,SubscribeForm
 from ..request import get_quotes
 
@@ -30,3 +30,32 @@ def index():
         return redirect(url_for('main.index'))
     
     return render_template('index.html', blogs = blogs,quotes =quotes,user=current_user, form = form)
+
+
+@main.route('/comment/<int:blog_id>', methods = ['POST','GET'])
+@login_required
+def comment(blog_id):
+    form = CommentForm()
+    blog = Blog.query.get(blog_id)
+    comments = Comment.query.filter_by(blog_id = blog_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        blog_id = blog_id
+        new_comment = Comment(comment = comment,blog_id = blog_id,user=current_user)
+        new_comment.save_comment()
+        return redirect(url_for('.comment', blog_id = blog_id))
+    
+    return render_template('comment.html', form = form, blog = blog,comments=comments)
+
+@main.route('/create_new', methods = ['POST','GET'])
+@login_required
+def new_blog():
+    form = BlogForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data         
+        new_blog = Blog(title = title,post=post,user=current_user)
+        new_blog.save_blog()
+        return redirect(url_for('main.index'))
+        
+    return render_template('new_post.html', form = form,title = "Your Blog"   )
