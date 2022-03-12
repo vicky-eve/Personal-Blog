c3c9e690bf734fc253b83a2dcf251ca79c1e6ae0
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 22:18:13 +0300

    created the manage.py file and set to development

---
+++ manage.py
@@ -0,0 +1,16 @@
+from app import create_app, db
+
+from app.models import User, Blog, Subscribe, Quote, Comment
+from flask_script import Manager, Server
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,User = User, Blog = Blog, Subscribe = Subscribe, Quote = Quote, Comment = Comment)
+    
+if __name__ == '__main__':
+    manager.run()
