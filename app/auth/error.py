761ec8257c28c84a1fd618b9bb8593cc5d58530d
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 22:10:19 +0300

    routin methods for login, registration and logout

---
+++ app/auth/error.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from . import auth
+
+@auth.app_errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
