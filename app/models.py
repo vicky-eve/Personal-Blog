c86415d7948b313fb2a5e06a99b52db546cc1701
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 19:25:44 +0300

    added login manager for the user class

--- app/models.py
+++ app/models.py
@@ -2,6 +2,11 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
