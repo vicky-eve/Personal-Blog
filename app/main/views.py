64c378003210f8832a0bf22c7086931c3dceea4a
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 21:14:15 +0300

    created route method for updating the profile of a user

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from flask_login import login_required,current_user
 from models import User, Blog, Quote,Subscribe, Comment
 from .forms import UpdateProfile,BlogForm,CommentForm,UpdateBlog,SubscribeForm
 from ..request import get_quotes
+from .. import db
 
 
 @main.route('/user/<uname>')
@@ -58,4 +59,20 @@ def new_blog():
         new_blog.save_blog()
         return redirect(url_for('main.index'))
         
-    return render_template('new_post.html', form = form,title = "Your Blog"   )
+    return render_template('new_post.html', form = form,title = "Your Blog")
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data  
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
