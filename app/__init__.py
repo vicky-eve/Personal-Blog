778069a45e6e1afd993034547bc608e220db0420
vicky <victoria.awuor@student.moringaschool.com>
2022-03-12 18:09:38 +0300

    installed sqlalchemy, bootstrap, mail, simplemde, uploads, login and initialised

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask import Flask
+from config import config_options
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+import os
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+simple = SimpleMDE()
+
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config['UPLOADED_PHOTOS_DEST'] = 'static/photos' 
+    app.config.from_object(config_options[config_name])
+    configure_uploads(app,photos)    #configure upload set
+     
+    #initialising the flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
--- app/config.py
+++ app/config.py
@@ -1,3 +1,15 @@
 import os 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vicky:aderazi@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
